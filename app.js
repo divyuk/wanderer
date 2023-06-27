92dbebb7502ab67c829a32c2720a2087dc94b1b7
divyuk <div14kes@gmail.com>
2023-06-27 16:22:39 +0530

    Adding the Post method

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-// app.get('/' , (req,res)=>{
-//     res.status(200).send("Hello from server");
-// })
+//! Middleware needed to access the body
+app.use(express.json());
 
 // Reading File in Synchronous way and not in callback as it may block the event loop
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
@@ -20,6 +19,22 @@ app.get('/api/v1/tours' , (req,res)=>{
 });
 
 
+// POST
+app.post('/api/v1/tours' , (req,res)=>{
+
+    const newId = tours[tours.length-1].id +1;
+    const newTour = Object.assign({id:newId} , req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err=>{
+            res.status(201).json({
+                status:'success',
+                data :{
+                    newTour
+                }
+            })
+    } )
+
+})
 
 const port = 3000;
 app.listen(port, (req,res)=>{
