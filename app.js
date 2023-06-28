f2f109496f9556109f613791436b1be85d720446
divyuk <div14kes@gmail.com>
2023-06-28 14:52:42 +0530

    Part 1 Routes| Refcatoring | Middleware

--- app.js
+++ app.js
@@ -1,63 +1,21 @@
-const fs = require('fs');
 const express = require('express');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
-
-//! Middleware needed to access the body
+//-------------------------//
+//! Middleware -> the below 2 are required for all the routes
 app.use(express.json());
-
-// Reading File in Synchronous way and not in callback as it may block the event loop
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-
-app.get('/api/v1/tours' , (req,res)=>{
-    res.status(200).json({
-        'status' : 'success',
-        'result' : tours.length,
-        data:{
-            tours
-        }
-})
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
 });
 
-// Get using id
-app.get('/api/v1/tours/:id' , (req,res)=>{
+//------------------------//
+// Mounting the Routers
+//? tourRouter is a middleware
+//? '/api/v1/tours' for this route we are applying tourRouter middleware
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-    const id = req.params.id * 1 // converting the string to number
-    const tour = tours.find(ele => ele.id === id);
-    console.log(tour)
-    if(tour){
-    res.status(200).json({
-        'status' : 'success',
-        data:{
-            tour
-        }
-    })}
-    else{
-        res.status(404).json({
-            'status' : 'fail',
-        })
-    }
-
-})
-
-
-// POST
-app.post('/api/v1/tours' , (req,res)=>{
-
-    const newId = tours[tours.length-1].id +1;
-    const newTour = Object.assign({id:newId} , req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err=>{
-            res.status(201).json({
-                status:'success',
-                data :{
-                    newTour
-                }
-            })
-    } )
-
-})
-
-const port = 3000;
-app.listen(port, (req,res)=>{
-    console.log(`Listining on port ${port}`);
-});
+module.exports = app; // for single exports
---
+++ routes/tourRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const tourController = require('../controllers/tourController');
+
+const router = express.Router(); //Defining our own ROUTE
+
+/* This is middleware for tourroute when id is specidfied if not this doesnt works.
+No matter what the route is, this is going to run as this middleware is specified before
+and order matter.*/
+// router.param('id' , (req,res,next,val)=>{
+//     console.log(`Tour id is ${val}`);
+//     next();
+// })
+
+router.param('id', tourController.checkID);
+
+// Routes for other HTTP requests
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour); // passes 1st from checkBody middleware then to createTour middleware.
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
