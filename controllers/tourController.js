e3a8615597f2e8833d67a0afa2dfc0147864f601
divyuk <div14kes@gmail.com>
2023-06-29 15:50:17 +0530

    get tour by id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,15 +29,21 @@ exports.getAllTours = async (req, res) => {
   }
 };
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  res.status(200).json({
-    status: 'success',
-    // data: {
-    //   tour
-    // }
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      staus: 'fail',
+      message: err.message,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
