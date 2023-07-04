a1d6e9e5d266408fd17f4407bafd43c30ea80310
divyuk <div14kes@gmail.com>
2023-07-04 11:58:56 +0530

    globalErrorHandler

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utilis/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 //-------------------------//
@@ -20,20 +22,10 @@ app.use('/api/v1/users', userRouter);
 
 // Handling the Unhandled Routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Cannot find ${req.originalUrl} on this server`,
-  });
+  next(new AppError(`Cannot find ${req.originalUrl} on this server`));
 });
 
 // Global error handler
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || '500';
-  err.status = err.status || 'fail';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app; // for single exports
